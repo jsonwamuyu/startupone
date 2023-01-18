da9a69ae60d66a5cf0c458b1f812c37886fd5e93
jsonwamuyu <jysonmuchiri@gmail.com>
2023-01-18 00:03:41 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{html,js,jsx,tsx,ts}",
+            "./src/**/*.{html,js,jsx,ts,tsx}",
+            "./pages/**/*.{html,js,jsx,tx,tsx}"
+          ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
